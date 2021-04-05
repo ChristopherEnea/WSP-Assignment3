d0b427c5be8b63f2a7f429b513fa6697f51a3c54
Christopher Enea <cenea@ltu.edu>
2021-04-04 23:38:39 -0400

    DateValidation mostly working

--- middleware/DateValidation.js
+++ middleware/DateValidation.js
@@ -17,13 +17,16 @@ function validateTime(incomingTime) {
   return false;
 }
 
+// todo. add error logging for bad parseInt
 router.all('*', async (request, response, next) => {
-  console.log('query:', request.query['date-validation']);
-  const test = Object.keys(request.query);
-  for (const [key, value] of Object.entries(test)) {
-    console.log(`${key}: ${value}`);
-  }
-  console.log('query:', test);
+  // console.log('query:', request.query['date-validation']);
+  Object.keys(request.query).forEach((key) => {
+    if (key.toLowerCase() === 'date-validation') {
+      console.log(request.query[key]);
+      console.log(validateTime(Number.parseInt(request.query[key], 10)));
+    }
+  });
+  // console.log('query:', test);
   // console.log('header:', request.get('date-validation'));
   // console.log(validateTime(request.get('date-validation')));
   if (!request.get('date-validation')) {
