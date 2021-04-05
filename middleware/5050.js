2a5b8698071879abbc6e03758412d4fbb35d0809
Christopher Enea <cenea@ltu.edu>
2021-04-05 17:37:43 -0400

    Removed next()

--- middleware/5050.js
+++ middleware/5050.js
@@ -1,11 +1,10 @@
 const coinflip = require('coinflip');
 const StatusCodes = require('http-status-codes');
 
-module.exports = async (request, response, next) => {
+module.exports = async (request, response) => {
   if (coinflip()) {
     response.status(StatusCodes.OK).send('Hello World');
   } else {
     response.status(StatusCodes.INTERNAL_SERVER_ERROR).send('oops');
   }
-  next();
 };
